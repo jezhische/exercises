ffa4d135f6a9df92c97972f7a02c383292851186
jezhische <jezhische@gmail.com>
2017-03-22 06:13:14 +0200

    probes of literals

---
+++ src/main/java/literals/Literals.java
@@ -0,0 +1,17 @@
+package literals;
+
+/**
+ * Created by Ежище on 21.03.2017.
+ */
+public class Literals {
+//    public void getSignature() {
+//        System.out.println(this.getClass().getSigners());
+//    }
+    public static void main(String[] args) {
+        System.out.println('\77');
+        System.out.println('\72');
+        System.out.println("\u0065bc\123");
+        System.out.println("\127");
+//        new Literals().getSignature();
+    }
+}
